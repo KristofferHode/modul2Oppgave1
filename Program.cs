45b8792faa1ebd724d58ecfa484b6af9ce7eac84
KristofferHode <kristoffer9.kodehode@gmail.com>
2025-11-06 09:38:09 +0100

    simple generic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿namespace modul2Oppgave1;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace modul2Oppgave1;
 
 class Program
 {
@@ -6,15 +10,13 @@ class Program
     {
         Console.WriteLine("Hello, World!");
 
+        Console.WriteLine(Max(32, 10));
+        Console.WriteLine(Max("beta", "alpha"));
+
     }
-    class GenericClass
+    public static T Max<T>(T a, T b) where T : IComparable<T>
     {
-        
-        
+        return a.CompareTo(b) > 0 ? a : b;
     }
-
-
-
-
-
+    
 }
