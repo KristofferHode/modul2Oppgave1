618fb0fab44930b7b91e26467e248f8746150aea
KristofferHode <kristoffer9.kodehode@gmail.com>
2025-11-06 09:16:14 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace modul2Oppgave1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+    }
+    class GenericClass
+    {
+        
+        
+    }
+
+
+
+
+
+}
